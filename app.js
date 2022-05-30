6f29d0bdd3c92168e5b263e3349a0d0f78947be0
kislo-Luis <luiskislo15@gmail.com>
2022-05-30 18:23:14 -0300

    feat

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express') // requerimos expres
 const app = express() // ejecutamos express dentro de la var app
 const path = require('path') // path para sendFile y para resolver rutas join resolve.
-app.listen( 3000,console.log('server running'))//declaramos el puerto para el server local
+app.listen( 3000,console.log('server running 3000'))//declaramos el puerto para el server local
 app.use(express.static('public'));//aca atraves de app y el metodo use static de express avisamos que vamos a usar imagenes staticas
 
 
