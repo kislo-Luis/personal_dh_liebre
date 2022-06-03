e114e796d3cdf5a5ed7ca7f67922ada194cf8bed
kislo-Luis <luiskislo15@gmail.com>
2022-06-02 23:27:14 -0300

    new add files

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const express = require('express') // requerimos expres
-const app = express() // ejecutamos express dentro de la var app
-const path = require('path') // path para sendFile y para resolver rutas join resolve.
-app.listen( 3000,console.log('server running 3000'))//declaramos el puerto para el server local
-app.use(express.static('public'));//aca atraves de app y el metodo use static de express avisamos que vamos a usar imagenes staticas
+const express = require('express') 
+const app = express() 
+const path = require('path') 
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+});
 
 
-app.get('/', (req, res)=>res.sendFile(path.join(__dirname,'views/home.html'))); // aca usamos path para usar index.html y que la ruta nos lleve
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>res.sendFile(path.join(__dirname,'views/home.html'))); 
+
+app.get('/register', (req, res) =>res.sendFile(path.join(__dirname, 'views/register.html')));
 
 
 
