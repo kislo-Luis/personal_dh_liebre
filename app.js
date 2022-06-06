a7daa70825334da9c7db8cc603d8619c5ed69956
kislo-Luis <luiskislo15@gmail.com>
2022-06-06 14:54:24 -0300

    [new files add]

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ app.listen(port, () => {
 });
 
 
-
+app.use(express.urlencoded({extended:false}));
 
 app.use(express.static('public'));
 
 app.get('/', (req, res)=>res.sendFile(path.join(__dirname,'views/home.html'))); 
 
-app.get('/register', (req, res) =>res.sendFile(path.join(__dirname, 'views/register.html')));
+app.get('/login.html', (req, res) =>res.sendFile(path.join(__dirname, 'views/login.html')));
+
+app.get('/register.html', (req, res) =>res.sendFile(path.join(__dirname, 'views/register.html')));
 
 
 
